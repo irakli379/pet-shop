93cd9c3a6c538fd879770da61fc3ef132a5fa816
unknown <dgaribashvili@gmail.com>
2024-08-31 10:35:24 +0400

    Add Styles

--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -3,9 +3,11 @@ import styles from "./MainPage.module.css"
 
 export default function MainPage() {
   return (
-    <div className={styles.div}>
+    <div >
       <PageNav />
-      <h1 className={styles.h1}>Main Page || Pet Shop</h1>
+      <div className={styles.div}>
+        <h1 className={styles.h1}>Main Page || Pet Shop</h1>
+      </div>
     </div>
   );
 }
